6c97616f8cc6fe6778ce4a3002cf07bde35eb7d3
duping <553240007@qq.com>
2021-02-02 17:57:01 +0800

    react css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './AppPortals';
+import App from './css/styled-components/index';
 import './demo/style.css'
 
 ReactDOM.render(
